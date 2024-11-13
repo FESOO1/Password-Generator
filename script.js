c24df681b6ce4f0f06356745520e603f0e5e415b
FES001 <firdavs7794948@gmail.com>
2024-11-14 01:54:49 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const generatePasswordBtn = document.querySelector('.input-button');
 const output = document.querySelector('.output');
-const sybmolsPassword = '!@#$%^&*';
+const sybmolsPassword = '!@#$%^&*()_+?><}|{~';
 const string = 'qwertyuiopasdfghjklzxcvbnm';
 let timesString = 8;
 let intervalRepeat = 0;
 let intervalStop;
+let passwordSaver;
 
 function generatePassword() {
     intervalStop = setInterval(() => {
-        if (intervalRepeat < 8) {
+        let mixing = Math.floor(Math.random() * 4);
+        if (intervalRepeat < 12) {
             intervalRepeat++;
 
-            
+            if (mixing === 0) {
+                passwordSaver = string[Math.floor(Math.random() * 26)].toLowerCase();
+            } else if (mixing === 1) {
+                passwordSaver = Math.floor(Math.random() * 9);
+            } else if (mixing === 2) {
+                passwordSaver = string[Math.floor(Math.random() * 26)].toUpperCase();
+            } else {
+                passwordSaver = sybmolsPassword[Math.floor(Math.random() * sybmolsPassword.length)];
+            }
+
+            output.textContent += passwordSaver;
         } else {
+            intervalRepeat = 0;
             clearInterval(intervalStop);
         };
-    }, 1000);
+    }, 10);
 };
 
-generatePasswordBtn.addEventListener('click', generatePassword);
+generatePasswordBtn.addEventListener('click', () => {
+    output.textContent = '';
+    generatePassword();
+});
